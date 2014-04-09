011336951cb0b45b6b150fa473e7e5e20e6d9faa
duoduo369 <duoduo3369@gmail.com>
2014-04-09 16:44:57 +0800

    fix 爬行规则

--- app/spiders/spiders/spiders/paper_edu_spider.py
+++ app/spiders/spiders/spiders/paper_edu_spider.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor as sle
+from scrapy.contrib.loader import ItemLoader
+from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.selector import Selector
-from spiders.items import PaperEduItem
+
 import spiders.misc.log as log
+from spiders.items import PaperEduItem
+
+URL_PREFIX = u'http://www.paper.edu.cn/advanced_search/resultHighSearch'
 
 class PaperEduSpider(CrawlSpider):
     name = 'paper_edu_spider'
@@ -12,43 +16,43 @@ class PaperEduSpider(CrawlSpider):
         Rule(
             sle(
                 allow=('/index.php/default/releasepaper/content/\d+',),
-                #restrict_xpaths=('//*[@id="right"]/span[@class="zm_text_jh"]/a',),
             ),
             callback='parse_content',
         ),
         Rule(
             sle(
-                allow=("\?type=0.*page=\d+",),
-                restrict_xpaths=('//*[@id="right"]/li[@class="zm_gjss_right_l1"]/li/a[4]',),
+                allow=("\?type=0.*page=\d+.*",),
+                restrict_xpaths=(
+                    u'//a[re:match(text(), "下一页")]',
+                ),
             ),
             follow=True,
-            callback='parse_list',
         ),
     )
 
     def __init__(self, start_date=None, end_date=None, per_pages=20, page=1):
+        '''
+            arguments:
+                start_date -- 2014-01-01
+                end_date -- 2014-01-02
+        '''
         super(PaperEduSpider, self).__init__()
-        url = (u'http://www.paper.edu.cn/advanced_search/resultHighSearch?'
-        'type=0&subject=&title=&author=&abstract=&keywords=&'
+        url = (
+         URL_PREFIX +
+        '?type=0&subject=&title=&author=&abstract=&keywords=&'
         'begin=%s&end=%s&y1=2014&m1=04&d1=02&y2=2014&m2=04&d2=09&'
         'star=&method=RELEVANCE&filename=&company=&language=0&'
         'p1=0&p2=0&p3=0&p4=0&p5=0&p6=0&r1=and&r2=and&r3=and&r4=and&r5=and&'
         'pagesize=%s&timeA=&userleft=&paperleft=&jiaocha=&1=1&page=%s',)[0]
         start_url = url % (start_date, end_date, per_pages, page)
         self.start_urls = [start_url]
-        print '=========='
-        print self.start_urls
-        print self.rules
 
     def parse_start_url(self, response):
+        '''
+            处理start url第一页
+        '''
         log.info('start url parse_content')
         log.info(response.url)
 
     def parse_content(self, response):
-        log.info('parse_content')
         log.info(response.url)
-
-    def parse_list(self, response):
-        log.info('parse_list')
-        log.info(response.url)
-
