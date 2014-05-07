2731747637d5bac2e2d445556a079abf014b6123
duoduo369 <duoduo3369@gmail.com>
2014-05-07 19:54:25 +0800

    配置文件加强延迟间隔等，增加蜘蛛健壮性

--- app/spiders/spiders/settings.py
+++ app/spiders/spiders/settings.py
@@ -37,8 +37,20 @@ ITEM_PIPELINES = {
     'spiders.pipelines.DjangoPipeline': 300,
 }
 
+# 下载器超时时间(单位: 秒)
+DOWNLOAD_TIMEOUT = 60
+
 # 下载延迟
-DOWNLOAD_DELAY = 0.15
+# http://scrapy-chs.readthedocs.org/zh_CN/latest/topics/practices.html
+# 设置下载延迟(2或更高)。参考 DOWNLOAD_DELAY 设置
+DOWNLOAD_DELAY = 2
+# 如果启用，当从相同的网站获取数据时，Scrapy将会等待一个随机的值
+# 使用0.5到1.5之间的一个随机值 * DOWNLOAD_DELAY 的结果作为等待间隔
+RANDOMIZE_DOWNLOAD_DELAY = True
+
+# 自动限速(AutoThrottle)扩展
+# http://scrapy-chs.readthedocs.org/zh_CN/latest/topics/autothrottle.html
+AUTOTHROTTLE_ENABLED = True
 
 LOG_LEVEL = 'INFO'
 LOG_FILE = 'log/paper_edu.log'
