7f8da6adcc639437d9d39ad1f69f28ddbf9c5dfc
duoduo369 <duoduo3369@gmail.com>
2014-05-24 12:14:48 +0800

    添加后台

--- app/tech_dict/sites/admin.py
+++ app/tech_dict/sites/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
-
+from sites.models import SiteRawData, KeyWordCN, KeyWordEN, StatRawData
 # Register your models here.
+
+class SiteRawDataAdmin(admin.ModelAdmin):
+        pass
+
+class KeyWordENAdmin(admin.ModelAdmin):
+        pass
+
+class KeyWordCNAdmin(admin.ModelAdmin):
+        pass
+
+class StatRawDataAdmin(admin.ModelAdmin):
+        pass
+
+admin.site.register(SiteRawData, SiteRawDataAdmin)
+admin.site.register(KeyWordCN, KeyWordCNAdmin)
+admin.site.register(KeyWordEN, KeyWordENAdmin)
+admin.site.register(StatRawData, StatRawDataAdmin)
